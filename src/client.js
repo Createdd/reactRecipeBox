b71dd5a41423fc9515a09200d69bdf5a05ea8ab1
Daniel Deutsch <deudan1010@gmail.com>
2016-12-10 16:53:46 +0100

    write header

--- src/client.js
+++ src/client.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Header extends React.Component {
+  render () {
+    return (
+      <div className='Header'>
+        <h1>Build A Recipe Box</h1>
+        <h2>written by Daniel Deutsch</h2>
+        <h4>A Free Code Camp Challenge</h4>
+      </div>
+    )
+  }
+}
+
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Header />,
   document.getElementById('root')
 );
